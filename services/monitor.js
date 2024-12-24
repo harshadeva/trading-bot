b58b2a1039861ce2cd7f88fafd0adf5d0cc5b98a
Harshadeva <harshadeva@webmotech.com>
2024-12-25 01:36:56 +0530

    remove logs

--- services/monitor.js
+++ services/monitor.js
@@ -20,7 +20,7 @@ async function monitorMarket(symbol,investingAmount) {
         const sellValue = currentPrice * quantity;
         const buyValue = buyPrice * quantity;
         const sellFee = sellValue * TRANSACTION_FEE_PERCENT;
-        const grossProfit = sellValue - buyValue;
+        const grossProfit = sellValue - activeBuy.buy_price;
         const profit = grossProfit - sellFee;
 
         console.log(
